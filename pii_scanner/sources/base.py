6e69204f71163624ef857b1ceec5e0863a3f6073
liranjp <59030525+liranjp@users.noreply.github.com>
2025-07-22 12:26:05 +0300

    Delete pii_scanner directory

--- pii_scanner/sources/base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Generator, Tuple
-
-class DataSource(ABC):
-    @abstractmethod
-    def read_data(self) -> Generator[Tuple[str, str], None, None]:
-        # Yields tuples of (data, source_context)
-        raise NotImplementedError
-
